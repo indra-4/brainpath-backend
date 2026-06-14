18df337fd93e8a814c812a7899cd47098104964e
BrainPath Backend Dev <capstone@brainpath.id>
2026-06-14 19:23:53 +0700

    fix: use Neon DB password SNI hack

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -45,9 +45,14 @@ if (isset($_ENV['VERCEL']) || getenv('VERCEL') == "1") {
     putenv('CACHE_STORE=array');
     putenv('QUEUE_CONNECTION=sync');
     
-    // Inject Neon PostgreSQL Credentials via DB_URL to support SNI workaround
+    // Inject Neon PostgreSQL Credentials (with SNI workaround in password)
     putenv('DB_CONNECTION=pgsql');
-    putenv('DB_URL=pgsql://neondb_owner:npg_k2vErFL1OICS@ep-noisy-night-aoaqai76.c-2.ap-southeast-1.aws.neon.tech:5432/neondb?sslmode=require&options=endpoint%3Dep-noisy-night-aoaqai76');
+    putenv('DB_HOST=ep-noisy-night-aoaqai76.c-2.ap-southeast-1.aws.neon.tech');
+    putenv('DB_PORT=5432');
+    putenv('DB_DATABASE=neondb');
+    putenv('DB_USERNAME=neondb_owner');
+    putenv('DB_PASSWORD=endpoint=ep-noisy-night-aoaqai76;npg_k2vErFL1OICS');
+    putenv('DB_SSLMODE=require');
 }
 
 return $app;
